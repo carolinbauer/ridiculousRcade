0d76ff0c7fd2b089acef5a9ff02682dfa1a645bb
Carolin Bauer <bcarolin36@gmail.com>
2023-07-22 16:47:57 +0200

    added rock paper scissors game

---
+++ R/rock_paper_scissors.R
@@ -0,0 +1,26 @@
+rock_paper_scissors <- function(choice) {
+  choice <- tolower(choice)
+  match.arg(arg = choice, choices = c("rock", "paper", "scissors"))
+  choice_opponent <- switch(sample(1:3, 1), "rock", "paper", "scissors")
+  if(choice == "scissors"){
+    if(choice_opponent == "scissors") result <- "It's a tie."
+    if(choice_opponent == "rock") result <- "You lost."
+    if(choice_opponent == "paper") result <- "You won!"
+  }
+  if(choice == "rock"){
+    if(choice_opponent == "rock") result <- "It's a tie."
+    if(choice_opponent == "paper") result <- "You lost."
+    if(choice_opponent == "scissors") result <- "You won!"
+  }
+  if(choice == "paper"){
+    if(choice_opponent == "paper") result <- "It's a tie."
+    if(choice_opponent == "scissors") result <- "You lost."
+    if(choice_opponent == "rock") result <- "You won!"
+  }
+  cat("You chose: ", choice, "\n",
+      "Your opponent chose: ", choice_opponent,  "\n",
+      result, sep = "")
+  
+}
+
+

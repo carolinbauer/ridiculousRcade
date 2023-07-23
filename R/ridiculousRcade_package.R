f57e038f53b09f75d198ec15ce8288eedbdb06e4
Carolin Bauer <bcarolin36@gmail.com>
2023-07-23 15:29:46 +0200

    added documentation

---
+++ R/ridiculousRcade_package.R
@@ -0,0 +1,12 @@
+#' Play Games in your Console
+#'
+#' This package contains two functions, tic_tac_toe() and rock_paper_scissors()
+#'
+#'
+#' @name ridiculousRcade
+#' @encoding UTF-8
+#' @docType package
+#' @author Carolin Bauer < bauer.carolin@@campus.lmu.de >
+#' @author Stefanie Halmburger <halmburger.stefanie@@campus.lmu.de>
+#'
+NULL
